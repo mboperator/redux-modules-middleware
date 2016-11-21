a74c462901fb8a15fdff397618e1660da0c5f2c1
Marcus Bernales <mboperator@gmail.com>
2016-11-20 19:30:26 -0800

    Added some common middleware

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { v4 } from 'uuid';
+
+export const decoratePayload = func => ({ payload, ...action }) =>
+  ({ payload: func(payload), ...action });
+
+export const decorateMeta = func => ({ meta, ...action }) =>
+  ({ meta: func(meta), ...action });
+
+const defaultErrorChecker = payload => (payload instanceof Error);
+
+export const parsePayloadErrors = (checkForError = defaultErrorChecker) =>
+  ({ payload, ...action }) => ({
+    ...action,
+    error: checkForError(payload),
+  });
+
+const defaultFormatter = (type, action) =>
+  `${type} | payload: ${action.payload} meta: ${action.meta} errors: ${action.errors}`
+
+export const log = (formatter = defaultFormatter) =>
+  ({ type, ...action }) => {
+    console.log(formatter(type, action));
+    return action;
+  };
+
+export const addUUID = (key = 'uuid') => action => ({
+  [key]: v4(),
+  ...action,
+});
